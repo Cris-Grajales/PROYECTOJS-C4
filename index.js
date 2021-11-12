2d471696e5c0f861c6300a27badd0d7b59007322
Cris-Grajales <ccgf5252@gmail.com>
2021-11-12 11:07:12 -0500

    entrar

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app=express();
+const bodyParser = require('body-parser');
+
+//especificamos el subdirectorio donde se encuentran las páginas estáticas
+app.use(express.static(__dirname + '/public'));
+
+//extended: false significa que parsea solo string (no archivos de imagenes por ejemplo)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/login', (req, res) => {
+        
+		let password=req.body.password;
+		let usuario=req.body.username;
+		if(usuario=="admininicial" && password=="admin123456") {
+			res.send("Holis admin");
+		}
+		else {
+			res.redirect('/index.html');
+		}
+})
+
+var server=app.listen(8081, () => {
+    console.log('Servidor web iniciado');
+  });
